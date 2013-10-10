72e6419c2141e9f938117cbc817fdefe0e911c01
Ivan Storck <ivanoats@gmail.com>
2013-10-10 09:01:18 -0700

    removed focus re #16

--- test/features/check_for_bootstrap_test.rb
+++ test/features/check_for_bootstrap_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 feature "As a developer I want a front-end framework so that I can easily modify the look and feel of my site" do
-  focus
   scenario "check for bootstrap" do
     visit root_path
     page.body.must_match /col-/
