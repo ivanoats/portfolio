dcbcbbc6d082410a4692948a06b3f04a57ab134e
Ivan Storck <ivanoats@gmail.com>
2013-10-12 22:25:51 -0700

    Reorganizing create and view project specs properly. Re #19

--- test/features/portfolio/view_project_test.rb
+++ test/features/portfolio/view_project_test.rb
@@ -2,13 +2,11 @@ require "test_helper"
 
 feature "As the site owner, I want to add a portfolio item so that I can show off my work" do
   scenario "adding a new project" do
+    # Given a a couple of projects (loaded from fixtures)
+    # When I visit /projects
     visit projects_path
-    click_on "New project"
-    fill_in "Project Name", with: "Code Fellows Portfolio"
-    fill_in "Technologies Used", with: "Rails, Ruby, Bootstrap, HTML5, CSS3"
-    click_on "Create Project"
-    page.text.must_have "Project was successfully added"
-    page.text.must_have "Code Fellows Portolio"
-    page.text.must_have "Rails"
+    # Then I should see a list of projects
+    page.text.must_include "Code Fellows Portolio"
+    page.text.must_include "Project Name Two"
   end
 end
