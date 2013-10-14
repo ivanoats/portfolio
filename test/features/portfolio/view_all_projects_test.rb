9cc764772fd64896b701cd96acf8c37aa5c5c5bf
Ivan Storck <ivanoats@gmail.com>
2013-10-13 23:22:49 -0700

    ability to view projects list implemented and specified. re #18

--- test/features/portfolio/view_all_projects_test.rb
+++ test/features/portfolio/view_all_projects_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
-feature "As the site owner, I want to add a portfolio item so that I can show off my work" do
-  scenario "adding a new project" do
+feature "As the site visitor, I want to see a developer's portfolio" do
+  scenario "viewing all projects" do
     # Given a a couple of projects (loaded from fixtures)
     # When I visit /projects
     visit projects_path
