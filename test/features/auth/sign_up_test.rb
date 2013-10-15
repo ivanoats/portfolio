23b3e7d68206467b1acf0f340bee3c772a6649cc
Ivan Storck <ivanoats@gmail.com>
2013-10-14 23:59:48 -0700

    Users can sign up, Closes #23

--- test/features/auth/sign_up_test.rb
+++ test/features/auth/sign_up_test.rb
@@ -1,18 +1,23 @@
 require "test_helper"
 
-feature(<<-EOF
+feature(<<-___
   As a web developer
   I want to be able to sign up for an account on my website
   so that I can perform actions that require me to be logged in.
-  EOF
+  ___
 ) do
 
   scenario "sign up" do
-    visit sign_up_path
+    # Given a user registration path
+    visit new_user_registration_path
+    # When I register with valid info
+    # fill_in "Name",  with: "Terry Tester"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
-    click_on "Create Account"
-    page.must_have_content "Sucessfully signed up"
+    fill_in "Password confirmation", with: "password"
+    click_on "Sign up"
+    # Then I should be signed up
+    page.must_have_content "Welcome! You have signed up successfully"
     page.wont_have_content "There was a problem with your sign up"
   end
 end
