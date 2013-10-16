86489f2372852b7442556c60886429beccbf84b8
Ivan Storck <ivanoats@gmail.com>
2013-10-15 22:33:01 -0700

    posts only editable by authenticated user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 # Blog posts controller
 class PostsController < ApplicationController
+
+  before_filter :authenticate_user!, except: [:show, :index]
+
   # GET /posts
   # GET /posts.json
   def index
