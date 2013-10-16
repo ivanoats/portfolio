14441a9b01b10974ac9bde074ecfe88b5215de15
Ivan Storck <ivanoats@gmail.com>
2013-10-16 11:26:43 -0700

    clean up test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,11 +19,13 @@ class ActiveSupport::TestCase
 
   # signs in a user
   def sign_in_user
-    @user = users(:one)
     visit new_user_session_path
-    fill_in "Email", with:  @user.email
+    fill_in "Email", with: users(:one).email
     fill_in "Password", with: "password"
     click_on "Sign in"
   end
+
 end
+
+
 
