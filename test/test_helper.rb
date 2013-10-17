869555c61bc9fd6dd64f89e54cdddca895b8afb9
Ivan Storck <ivanoats@gmail.com>
2013-10-16 21:32:25 -0700

    adding coveralls support

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
 SimpleCov.start 'rails'
+require 'coveralls'
+Coveralls.wear!('rails')
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
