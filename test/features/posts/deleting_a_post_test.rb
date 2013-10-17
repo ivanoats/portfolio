403060939bfbb5426f05400fb732ece0b93f7164
Ivan Storck <ivanoats@gmail.com>
2013-10-16 20:39:22 -0700

    An editor can delete anyone's posts. Closes #30

--- test/features/posts/deleting_a_post_test.rb
+++ test/features/posts/deleting_a_post_test.rb
@@ -1,5 +1,20 @@
 require "test_helper"
 
+# sets up a test post by an author
+# returns a post_id
+def set_up_a_post_by_author
+  # Given a logged in author
+  sign_in("author")
+  # And a post by that author
+  visit posts_path
+  click_on "New Post"
+  fill_in "Title", with: "author's title destroy test"
+  fill_in "Body",  with: "author's body"
+  click_on "Create Post"
+  # figure out the post id from the page url
+  post_id = current_url.split('/').last
+end
+
 feature "Deleting a Post" do
   scenario "post is deleted with a click" do
     # Given an existing post, and an authenticated user
@@ -18,18 +33,26 @@ feature "Deleting a Post" do
   end
 
   scenario "an author can delete her own unpublished posts" do
-    # Given a logged in author
-    sign_in("author")
-    # And a post by that author
-    visit posts_path
-    click_on "New Post"
-    fill_in "Title", with: "author's title destroy test"
-    fill_in "Body",  with: "author's body"
-    click_on "Create Post"
-    # figure out the post id from the page url
-    post_id = current_url.split('/').last
+    post_id = set_up_a_post_by_author
     click_on "Back"
     click_link "Destroy", href: "/posts/#{post_id}"
+    # Then I should see a message about the post being deleted
+    page.must_have_content "Post was deleted"
+    # And the post title should not be in the index
+    page.wont_have_content "author's title destroy test"
+  end
+
+  scenario "an editor can delete anyone's posts" do
+    # Given a post by another user
+    # And a logged in editor
+    # When I click on destroy link for that post
+    # Then I should see a message about the post being deleted
+    # And the post title should not be in the index
+    post_id = set_up_a_post_by_author
+    click_link "Sign Out"
+    sign_in "editor"
+    visit posts_path
+    click_link("Destroy", href: "/posts/#{post_id}")
     # Then I should see a message about the post being deleted
     page.must_have_content "Post was deleted"
     # And the post title should not be in the index
