4993c0dac356595faf0981a63ac39371806272a0
Ivan Storck <ivanoats@gmail.com>
2013-10-20 09:20:04 -0700

    twitter sign in confirms with user.name (nickname)

--- test/features/auth/sign_in_test.rb
+++ test/features/auth/sign_in_test.rb
@@ -26,8 +26,13 @@ feature "
    OmniAuth.config.test_mode = true
    Capybara.current_session.driver.request.env['devise.mapping'] = Devise.mappings[:user]
    Capybara.current_session.driver.request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
-   OmniAuth.config.add_mock(:twitter, {uid: '12345', nickname: 'test_twitter_user'})
+   OmniAuth.config.add_mock(:twitter,
+                            {
+                            uid: '12345',
+                            info: { nickname: 'test_twitter_user'},
+                            })
    click_on "Sign in with Twitter"
-   page.must_have_content "Signed in!"
+   save_and_open_page
+   page.must_have_content "test_twitter_user, you are signed in!"
   end
 end
