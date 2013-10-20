ee5c7f98e7a29da768422e54cc5fb38fd5858d07
Ivan Storck <ivanoats@gmail.com>
2013-10-20 15:56:21 -0700

    cleanup of css and comments

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class OmniauthCallbacksController < ApplicationController
   def all
-    # raise request.env["omniauth.auth"].to_yaml
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
       flash.notice = "#{user.name}, you are signed in!"
