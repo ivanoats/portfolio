81e374c4f231dd8d9ac7847dc0ba431ee1abf329
Ivan Storck <ivanoats@gmail.com>
2013-10-20 08:31:34 -0700

    wip on omniauth - stuck on new request hash format change

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Portfolio::Application.routes.draw do
 
   devise_for :users,
              controllers: {registrations: "users/registrations",
-                           passwords: "users/passwords"}
+                           passwords: "users/passwords",
+                           omniauth_callbacks: "omniauth_callbacks"}
 
   resources :posts
   resources :projects
