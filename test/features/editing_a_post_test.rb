7545c977fe3c02d691f01fa6e9f4a75a78e15605
Brook Riggio <brooke.riggio@gmail.com>
2014-01-15 15:59:23 -0800

    Add fixture. Use in specs.

--- test/features/editing_a_post_test.rb
+++ test/features/editing_a_post_test.rb
@@ -3,16 +3,15 @@ require "test_helper"
 feature "Editing a Post" do
   scenario "submit updates to an existing post" do
     # Given an existing post
-    post = Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
-    visit post_path(post)
+    visit post_path(posts(:cr))
 
     # When I click edit and submit changed data
     click_on "Edit"
-    fill_in "Title", with: "Becoming a Web Developer"
+    fill_in "Body", with: "Becoming a Web Developer"
     click_on "Update Post"
 
     # Then the post is updated
-    page.text.must_include "Post was successfully updated"
-    page.text.must_include "Web Developer"
+    page.text.must_include posts(:cr).title
+    page.text.must_include "Becoming a Web Developer"
   end
 end
