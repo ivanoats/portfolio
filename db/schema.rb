1ca6e29b991ab1260264d36f62923feac1d51bf1
Brook Riggio <brooke.riggio@gmail.com>
2014-01-15 00:19:25 -0800

    Fix attribute naming.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107045642) do
+ActiveRecord::Schema.define(version: 20140115072457) do
 
   create_table "posts", force: true do |t|
-    t.string   "name"
     t.string   "title"
-    t.text     "content"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
