09c559fd273739d1fbccdb01a9128b43042d5435
Brook Riggio <brooke.riggio@gmail.com>
2014-01-20 00:32:16 -0800

    Add edit behavior to spec

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,12 +13,27 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      # we'll get to this in a bit
+      flash.now[:error] =  "Project could not be saved."
+      render :new
     end
   end
 
   def show
     @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update_attributes(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
   end
 
 private
