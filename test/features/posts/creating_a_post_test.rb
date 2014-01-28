658ca325ab329c5d2ad5e2393228050bacd4ba58
Brook Riggio <brooke.riggio@gmail.com>
2014-01-27 23:33:47 -0800

    Add basic authorization for Posts.

--- test/features/posts/creating_a_post_test.rb
+++ test/features/posts/creating_a_post_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "Creating a post" do
   scenario "submit form data to create a new post" do
     # Given an authorized user complets a new post format
-    sign_in
+    sign_in(:author)
     visit new_post_path
 
     fill_in "Title", with: posts(:cr).title
@@ -12,11 +12,47 @@ feature "Creating a post" do
     # When I submit the form
     click_on "Create Post"
 
-    # Then a new post should be created and displayed
+    # Then a new unpublished post should be created and displayed
     page.text.must_include posts(:cr).title
     page.text.must_include posts(:cr).body
-    page.has_css? "#author"
-    page.text.must_include users(:eli).email # Use your fixture name here.
+    assert page.has_css? "#author"
+    page.text.must_include users(:author).email
+    page.text.must_include "Status: Unpublished"
+  end
 
+  scenario "unauthenticated site visitors cannot visit new_post_path" do
+    visit new_post_path
+    page.must_have_content "You need to sign in or sign up before continuing"
+  end
+
+  scenario "authors can't publish" do
+    # Given an author's account
+    sign_in(:author)
+
+    # When I visit the new page
+    visit new_post_path
+
+    # There is no checkbox for published
+    page.wont_have_field('Published')
+  end
+
+  scenario "editors can publish" do
+    # Given an editor's account
+    sign_in(:editor)
+
+    # When I visit the new page
+    visit new_post_path
+
+    # There is a checkbox for published
+    page.must_have_field('Published')
+
+    # When I submit the form
+    fill_in "Title", with: posts(:cr).title
+    fill_in "Body", with: posts(:cr).body
+    check "Published"
+    click_on "Create Post"
+
+    # Then the published post should be shown
+    page.text.must_include "Status: Published"
   end
 end
