d6e8831a944d17357822abb923d75dd27ed68e9e
Ivan Storck <ivanoats@gmail.com>
2014-05-18 14:49:39 -0700

    Fixed GET index test https://app.asana.com/0/11169835259596/11348159604578

--- test/controllers/post_api_test.rb
+++ test/controllers/post_api_test.rb
@@ -5,19 +5,37 @@ describe PostsController do
 
   setup do
     @request.env['devise.mapping'] = Devise.mappings[:admin]
+
+    # Clear out all Post fixtures
+    Post.delete_all
+
+    # Set up two Posts
+    @title1 = Faker::Name.name
+    @body1  = Faker::Lorem.paragraph
+
+    @title2 = Faker::Name.name
+    @body2  = Faker::Lorem.paragraph
+
+    @title3 = Faker::Name.name
+    @body3  = Faker::Lorem.paragraph
+
+    Post.create!(title: @title1, body: @body1, published: true)
+    Post.create!(title: @title2, body: @body2, published: true)
+    Post.create!(title: @title3, body: @body3, published: false)
   end
 
   describe 'GET /posts' do
     it 'returns all the published posts' do
-      # fixtures?
-      get :index, {} , 'Accept' => 'application/json'
+
+      get :index, format: :json
 
       response.status.must_equal 200
 
       body = JSON.parse(response.body)
       post_titles = body.map { |m| m['title'] }
-
-      expect(post_titles).to match_array(['Code Rails', 'Intro to HTTP'])
+      post_titles.must_include @title1
+      post_titles.must_include @title2
+      post_titles.wont_include @title3
     end
   end
 end
